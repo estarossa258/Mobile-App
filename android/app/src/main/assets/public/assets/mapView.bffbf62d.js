18dda0877aed3d16ae7aa613c02165a96b6393e6
estarossa258 <gjcabual@gmail.com>
2024-05-16 21:41:16 +0800

    rev2

--- android/app/src/main/assets/public/assets/mapView.bffbf62d.js
+++
@@ -1,8 +0,0 @@
-import "./main.59d65ffb.js";
-/* empty css                */function performSearch(event) {
-  event.preventDefault();
-  var searchQuery = document.getElementById("searchInput").value;
-  console.log("Performing search for:", searchQuery);
-  document.getElementById("searchInput").value = "";
-}
-document.getElementById("searchButton").addEventListener("click", performSearch);
